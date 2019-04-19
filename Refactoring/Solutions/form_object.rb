c37f95158effea22807482ed0452d5c33d0fa331
hainguyen-iwa <hai@iwa.fi>
2019-04-19 11:40:54 +0700

    add solution form_object

---
+++ Refactoring/Solutions/form_object.rb
@@ -0,0 +1,16 @@
+module Solutions
+  class FormObject
+    def usage
+      <<~USAGE.strip
+        1. keep business out of Controllers and Views
+        2. add validation support to plain old Ruby objects
+        3. display form validation errors using Rails conventions.
+        4. set the stage for `Extracted Validator`
+      USAGE
+    end
+
+    def apply_for(*code_smells)
+    end
+  end
+end
+
--- Refactoring/Solutions/replace_mixin_with_composition.rb
+++ Refactoring/Solutions/replace_mixin_with_composition.rb
@@ -15,14 +15,8 @@ module Solutions
       <<~REFERENCE.strip
         1. https://engineering.entelo.com/understanding-inheritance-and-composition-in-ruby-edc46c0f96c7
         2. this link help us when to choose inheritance approach or composition approach
-          - to use inheritance for is_a needed
-            + man is a human
-          - to use composition for has_a needed
-            + man can become a senior developer
       REFERENCE
     end
   end
-
   # puts ReplaceMixinWithComposition.new.usage
 end
-

57852605584d667131ea17105b6875a95f5ff60a
hainguyen-iwa <hai@iwa.fi>
2019-05-16 13:42:45 +0700

    add rails tools

---
+++ Security/Cryptography/brute_force_attack.rb
@@ -0,0 +1,15 @@
+module Security
+  module Cryptography
+    class BruteForceAttack
+      def define
+        <<~DEFINE.strip
+          a brute-force attack consists of an attacker
+          submitting many passwords or passphrases with the hope
+          of eventually guessing correctly.
+        DEFINE
+      end
+    end
+
+    # puts BruteForceAttack.new().define
+  end
+end

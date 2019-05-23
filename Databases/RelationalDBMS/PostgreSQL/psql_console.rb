a918599993812d3d747ab7efab81775c05435dd5
hainguyen-iwa <hai@iwa.fi>
2019-05-23 11:02:22 +0700

    add show config file

--- Databases/RelationalDBMS/PostgreSQL/psql_console.rb
+++ Databases/RelationalDBMS/PostgreSQL/psql_console.rb
@@ -1,11 +1,19 @@
 module RelationalDBMS
   module PostgreSQL
     class PsqlConsole
+      def show_config_file
+        conntect_db
+        Command.new('find a configuration file', 'SHOW config_file;')
+      end
+
+      private
+
       def conntect_db
         Command.new('connect to database', 'psql -U postgres')
       end
     end
 
     Command = Struct.new(:title, :command)
+    # puts PsqlConsole.new().show_config_file
   end
 end

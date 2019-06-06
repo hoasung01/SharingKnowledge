f0fe84db4411f544c4fdd5696ede8143c7af20ac
hainguyen-iwa <hai@iwa.fi>
2019-06-06 14:56:52 +0700

    add service management throwdown categories

---
+++ Computer/OperatingSystems/Linux/service_management_throwdown.rb
@@ -0,0 +1,9 @@
+module Computer
+  module OperatingSystems
+    module Linux
+      class ServiceManagementThrowdown
+        CATEGORIES = %w(Systemd SystemV Upstart).freeze
+      end
+    end
+  end
+end

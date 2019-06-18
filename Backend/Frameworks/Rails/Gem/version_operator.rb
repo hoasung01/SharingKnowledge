59256938e63c7292717e7646d38fc264dc3e3bf3
hainguyen-iwa <hai@iwa.fi>
2019-06-18 23:34:59 +0700

    re-organize topic structure

---
+++ Backend/Frameworks/Rails/Gem/version_operator.rb
@@ -0,0 +1,15 @@
+module Frameworks
+  module Rails
+    module Gem
+      class VersionOperator
+        def define
+          <<~SAMPLE.strip
+            '~1.0' => [1.0, 2.0]
+            '~> 1.5.0' => [1.5.0, 1.6.0]
+            '~> 1.5.3' => [1.5.3, 1.6.0]
+          SAMPLE
+        end
+      end
+    end
+  end
+end

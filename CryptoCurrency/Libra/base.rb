913e30cdddc91d1b4d4c60204ae8ecfd12c65ee6
hainguyen-iwa <hai@iwa.fi>
2019-06-18 22:23:05 +0700

    add libra crypto currency reference link

---
+++ CryptoCurrency/Libra/base.rb
@@ -0,0 +1,12 @@
+module CryptoCurrency
+  module Libra
+    class Base
+      def reference
+        <<~REFERENCE.strip
+          https://libra.org
+        REFERENCE
+      end
+
+    end
+  end
+end

483240eb694eda2d49a13474f5bac54703045967
hainguyen-iwa <hai@iwa.fi>
2019-06-19 11:02:01 +0700

    add definition of bash

---
+++ Computer/OperatingSystems/Unix/Bash/base.rb
@@ -0,0 +1,26 @@
+module Computer
+  module OperatingSystems
+    module Unix
+      module Bash
+        class Base
+          def define
+            <<~DEFINE.strip
+              A Unix shell is a command-line interpreter or
+              shell that provides a command line user interface
+              for Unix-like operating systems. The shell is both
+              an interactive command language and a scripting language,
+              and is used by the operating system to control the
+              execution of the system using shell scripts.
+            DEFINE
+          end
+
+          def reference
+            <<~REFERENCE.strip
+              https://en.wikipedia.org/wiki/Bash_(Unix_shell)
+            REFERENCE
+          end
+        end
+      end
+    end
+  end
+end

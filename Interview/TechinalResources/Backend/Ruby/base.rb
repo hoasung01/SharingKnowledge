7c862e5f55407ddb404e85063dbf6b714d18f7d4
hainguyen-iwa <hai@iwa.fi>
2019-06-20 10:26:37 +0700

    add technical interview resource

---
+++ Interview/TechinalResources/Backend/Ruby/base.rb
@@ -0,0 +1,18 @@
+module Interview
+  module TechnicalResources
+    module Backend
+      module Ruby
+        class Base
+          REFERENCES_LIST = [
+            junior: [
+              'https://rubygarage.org/blog/how-to-interview-your-ruby-on-rails-developer'
+            ],
+            senior: [
+              'https://resources.workable.com/senior-ruby-developer-interview-questions'
+            ]
+          ]
+        end
+      end
+    end
+  end
+end
---
+++ Interview/TechinalResources/Backend/base.rb
@@ -0,0 +1,11 @@
+module Interview
+  module TechnicalResources
+    module Backend
+      class Base
+        REFERENCES_LIST = [
+          'https://github.com/arialdomartini/Back-End-Developer-Interview-Questions'
+        ]
+      end
+    end
+  end
+end

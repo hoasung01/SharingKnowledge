b7f6dbe9f7b760017969d738ce9295197b6a6dd8
hainguyen-iwa <hai@iwa.fi>
2019-07-03 10:33:56 +0700

    add #til: find where a method defined

---
+++ Backend/ProgrammingLanguage/Ruby/TIL/Y2019/find_where_method_defined.rb
@@ -0,0 +1,14 @@
+module ProgrammingLanguage
+  module Ruby
+    module TIL
+      module Y2019
+        def find_where_method_defined
+          Example.new.method(:method).source_location
+          # => ["(irb)", 11]
+        end
+
+        class Example; def method() end end
+      end
+    end
+  end
+end

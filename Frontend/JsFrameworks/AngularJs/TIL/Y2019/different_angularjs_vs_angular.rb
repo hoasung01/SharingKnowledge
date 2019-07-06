9ab5ff4cbfb18263048cfc09209bf901fa90cadf
hainguyen-iwa <hai@iwa.fi>
2019-07-06 13:45:40 +0700

    what differences btw angularjs and angular

---
+++ Frontend/JsFrameworks/AngularJs/TIL/Y2019/different_angularjs_vs_angular.rb
@@ -0,0 +1,15 @@
+module Frontend
+  module JsFrameworks
+    module AngularJs
+      module TIL
+        module Y2019
+          def different_angularjs_vs_angular
+            <<~REFERENCE.strip
+              https://www.educba.com/angular-js-vs-angular/
+            REFERENCE
+          end
+        end
+      end
+    end
+  end
+end

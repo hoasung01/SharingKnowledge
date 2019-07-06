b627a12b814fbd046b0fbd0eca61341dbe5adba8
hainguyen-iwa <hai@iwa.fi>
2019-07-06 18:00:05 +0700

    can not undo git clean untracked directory or file

---
+++ Git/TIL/Y2019/cannot_undo_gclean.rb
@@ -0,0 +1,11 @@
+module Git
+  module TIL
+    module Y2019
+      def cannot_undo_gclean
+        <<~WARNING.strip
+          can not undo untracked directory/file after git clean
+        WARNING
+      end
+    end
+  end
+end

33fedbd789ed783bea0117add813370d0c080a76
hainguyen-iwa <hai@iwa.fi>
2019-07-09 16:30:34 +0700

    add complexity and definition for time

---
+++ Algorithm/Complexity/Time/describe.rb
@@ -0,0 +1,26 @@
+module Algorithm
+  module Complexity
+    module Time
+      class Describe
+        Factor = Struct.new(:context)
+
+        FACTORS = [
+          Factor.new('kỹ năng lập trình'),
+          Factor.new('Chương trình dịch mã nguồn'),
+          Factor.new('Tốc độ xử lý của vi xử lý'),
+          Factor.new('Bộ dữ liệu đầu vào')
+        ]
+
+        def self.yourself
+          <<~DESCRIBE.strip
+            là một khái niệm liên quan đến tốc độ
+            nhanh chậm của thuật toán khi nó thực thi:
+            #{FACTORS}
+          DESCRIBE
+        end
+
+        # puts Describe.yourself
+      end
+    end
+  end
+end

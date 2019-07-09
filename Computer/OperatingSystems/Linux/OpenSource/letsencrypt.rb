38d2c07194fdbceb0016a81ead8680280936f626
hainguyen-iwa <hai@iwa.fi>
2019-07-09 09:21:06 +0700

    add letsencrypt reference

---
+++ Computer/OperatingSystems/Linux/OpenSource/letsencrypt.rb
@@ -0,0 +1,17 @@
+module Computer
+  module OperatingSystems
+    module Linux
+      module OpenSource
+         class Letsencrypt
+          def define
+            <<~DEFINE.strip
+              To enable HTTPS on your website.
+              Let’s Encrypt is a CA (Certificate Authority)
+              https://letsencrypt.org
+            DEFINE
+          end
+        end
+      end
+    end
+  end
+end

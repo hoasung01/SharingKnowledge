c31626bd4872565691e74151a8df266f90b12be8
hainguyen-iwa <hai@iwa.fi>
2019-07-11 11:13:20 +0700

    add religion definition from wiki

---
+++ SocialLife/Religion/base.rb
@@ -0,0 +1,14 @@
+module SocialLife
+  module Religion
+    class Base
+      def definition
+        <<~DEFINITION.strip
+          là một hệ thống văn hoá các hành vi và thực hành được chỉ định,
+          quan niệm về thế giới, các kinh sách, địa điểm linh thiêng, lời tiên tri,
+          đạo đức, hoặc tổ chức liên quan đến tổ chức siêu nhiên, siêu việt hoặc tâm linh.
+          ( wiki )
+        DEFINITION
+      end
+    end
+  end
+end

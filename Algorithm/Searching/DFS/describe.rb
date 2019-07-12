d2ace5fffbf949b41a7fa04ad9270c1b93be9a3b
hainguyen-iwa <hai@iwa.fi>
2019-07-12 18:15:21 +0700

    update description

--- Algorithm/Searching/DFS/describe.rb
+++ Algorithm/Searching/DFS/describe.rb
@@ -4,8 +4,8 @@ module Algorithm
       class Describe
         def self.yourself
           <<~DESCRIBE.strip
-            - Deapth First Search là thuật toán tìm kiếm đồ thị
-            vô hướng hoặc có hướng, không trọng số
+            - Deapth First Search là thuật toán tìm kiếm theo chiều sâu
+            trên đồ thị vô hướng hoặc có hướng, không trọng số
             - DFS luôn tìm kiếm được đường đi từ một đỉnh bất kỳ cho trước
             tới một đỉnh đích ( nếu 2 đỉnh thuộc cùng thành phần liên thông
             với nhau). Nhưng không chắc chắn đường đi sẽ là đường đi ngắn nhất

ec7741ead07f29186eaa89fe04e3e255593ae408
Hai <hai@iwa.fi>
2019-08-05 11:53:37 +0700

    show content in words file

--- SocialLife/Vietnamese/dictionary.rb
+++ SocialLife/Vietnamese/dictionary.rb
@@ -2,8 +2,15 @@ module SocialLife
   module Vietnamese
     class Dictionary
       def show
-        # TODO
+        current_path = File.expand_path File.dirname(__FILE__)
+
+        File.open("#{current_path}/words.md", 'r') do |f|
+          f.each_line do |line|
+            puts line
+          end
+        end
       end
     end
+    print Dictionary.new().show
   end
 end

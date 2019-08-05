e3f9881a75d4f6307e0c13139b84fd6410e99080
Hai <hai@iwa.fi>
2019-08-05 10:25:07 +0700

    add tool httrack to download WWW to a local directory

---
+++ Backend/Frameworks/Rails/Tools/httrack.rb
@@ -0,0 +1,19 @@
+module Frameworks
+  module Rails
+    module Tools
+      class Httrack
+        def self.yourself
+          <<~DESCRIBE.strip
+            It allows you to download a World Wide Web
+            site from the Internet to a local directory,
+            building recursively all directories,
+            getting HTML, images, and other files
+            from the server to your computer
+
+            [reference: https://www.httrack.com/]
+          DESCRIBE
+        end
+      end
+    end
+  end
+end

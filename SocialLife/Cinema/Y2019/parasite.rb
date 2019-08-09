8a67cce24d37d301932b16f7114497d2642772fa
Hai <hai@iwa.fi>
2019-08-09 12:20:51 +0700

    refactor code for cinema

--- SocialLife/Cinema/Y2019/parasite.rb
+++ SocialLife/Cinema/Y2019/parasite.rb
@@ -2,22 +2,48 @@ module SocialLife
   module Cinema
     module Y2019
       class Parasite
+        LINE_NUMBER = 16
+
+        FILES = {
+          part1: 'part1.md',
+          part2: 'part2.md',
+          part3: 'part3.md',
+          part4: 'part4.md'
+        }.freeze
+
         def info
           director = Director.new('Bong Joon-ho')
         end
 
         def reviews
+          puts '*' * LINE_NUMBER
+          puts "Director: #{info.name}"
+          puts '====Reviews:===='
+
+          FILES.each do |key, value|
+            read_from("#{current_path}/Parasite/#{value}")
+          end
+
+          puts '*' * LINE_NUMBER
         end
 
         Director = Struct.new(:name)
 
         private
 
-        def files
-          Dir.glob("*.md") do |filename|
+        def current_path
+          File.expand_path("../../Reviews", __FILE__)
+        end
+
+        def read_from(file_path)
+          File.open(file_path, 'r') do |f|
+            f.each_line do |line|
+              puts line
+            end
           end
         end
       end
+      print Parasite.new().reviews
     end
   end
 end

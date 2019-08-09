67c83e3c7078d5f143d715df1c7e64ace7055fb6
Hai <hai@iwa.fi>
2019-08-09 11:41:42 +0700

    refactor code for vietnamese dictionary

--- SocialLife/Vietnamese/dictionary.rb
+++ SocialLife/Vietnamese/dictionary.rb
@@ -1,16 +1,27 @@
 module SocialLife
   module Vietnamese
     class Dictionary
-      def show
-        current_path = File.expand_path File.dirname(__FILE__)
+      DICTIONARY_FILE = 'words.md'.freeze
 
-        File.open("#{current_path}/words.md", 'r') do |f|
+      def show
+        read_from("#{current_path}/#{DICTIONARY_FILE}")
+      end
+
+      private
+
+      def read_from(file_path)
+        File.open(file_path, 'r') do |f|
           f.each_line do |line|
             puts line
           end
         end
       end
+
+      def current_path
+        File.expand_path File.dirname(__FILE__)
+      end
     end
-    print Dictionary.new().show
+
+    # print Dictionary.new().show
   end
 end

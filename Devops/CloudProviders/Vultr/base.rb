5187dcfa16e05de8314d39085c11633456db9a44
Hai <hai@iwa.fi>
2019-08-19 09:06:37 +0700

    add vultr cloud provider

---
+++ Devops/CloudProviders/Vultr/base.rb
@@ -0,0 +1,13 @@
+module Devops
+  module CloudProviders
+    module Vultr
+      class Base
+        def reference
+          <<~REFERENCE.strip
+            https://www.vultr.com/
+          REFERENCE
+        end
+      end
+    end
+  end
+end

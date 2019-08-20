806081679034c3a3182c18c20b24abc7f021f98c
Hai <hai@iwa.fi>
2019-08-20 15:01:27 +0700

    add health check monitoring as TIL

---
+++ Devops/TIL/Y2019/health_check_monitoring.rb
@@ -0,0 +1,16 @@
+module Devops
+  module TIL
+    module Y2019
+      class HealthCheckMonitoring
+        def reference
+          <<~REFERENCE.strip
+            [
+              https://medium.com/@tinchorb/health-check-monitoring-55e39715c921,
+              okcomputer, rails gem
+            ]
+          REFERENCE
+        end
+      end
+    end
+  end
+end

3f31c0ea6d79fad16e08a32cf6dae5316af7e29a
Hai <hai@iwa.fi>
2019-08-20 17:06:52 +0700

    add ruby metaprogramming reference

---
+++ Backend/ProgrammingLanguage/Ruby/MetaProgramming/base.rb
@@ -0,0 +1,13 @@
+module ProgrammingLanguage
+  module Ruby
+    module MetaProgramming
+      class Base
+        def reference
+          <<~REFERENCE.strip
+            http://ruby-metaprogramming.rubylearning.com/html/ruby_metaprogramming_2.html
+          REFERENCE
+        end
+      end
+    end
+  end
+end

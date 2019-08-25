9f715103f1fbf8b0d287519eda774030954cf7bc
Hai <hai@iwa.fi>
2019-08-25 12:25:10 +0700

    add json_server as fake rest api

---
+++ Backend/ProgrammingLanguage/Tools/json_server.rb
@@ -0,0 +1,13 @@
+module ProgrammingLanguage
+  module Tools
+    class JsonServer
+      def self.yourself
+        <<~DESCRIBE.strip
+          Get a full fake REST API with zero coding
+          in less than 30 seconds (seriously)
+          [reference: https://github.com/typicode/json-server]
+        DESCRIBE
+      end
+    end
+  end
+end

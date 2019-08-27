40c509093c604cf4578b77f6450621356a996ecd
Hai <hai@iwa.fi>
2019-08-27 11:15:03 +0700

    add service branches (quân chủng) in military

---
+++ SocialLife/Military/ServiceBranches/catalog.rb
@@ -0,0 +1,15 @@
+module SocialLife
+  module Military
+    module ServiceBranches
+      class Catalog
+        LIST = {
+          army: 'Lục quân',
+          navy: 'Hải quân',
+          marine_corps: 'Thủy quân lục chiến',
+          coast_guard: 'Tuần duyên',
+          air_force: 'Không quân'
+        }
+      end
+    end
+  end
+end

fd091ec2cb25dd4c79f05efb3cea15bdea804416
Hai <hai@iwa.fi>
2019-09-18 18:41:16 +0700

    add review to it_chap2 film

--- SocialLife/Cinema/Y2019/it_chap2.rb
+++ SocialLife/Cinema/Y2019/it_chap2.rb
@@ -2,12 +2,44 @@ module SocialLife
   module Cinema
     module Y2019
       class ItChap2
+        LINE_NUMBER = 16
+
+        FILES = {
+          file_name: 'myself_lession.md',
+        }.freeze
+
         def info
           director = Director.new('Andrés Muschietti')
         end
 
+        # TODO: refactor duplicated code
+        def reviews
+          puts '*' * LINE_NUMBER
+          puts "Director: #{info.name}"
+          puts '====Reviews:===='
+          FILES.each do |key, value|
+            read_from("#{current_path}/ItChap2/#{value}")
+          end
+          puts '*' * LINE_NUMBER
+        end
+
         Director = Struct.new(:name)
+
+        private
+
+        def current_path
+          File.expand_path("../../Reviews", __FILE__)
+        end
+
+        def read_from(file_path)
+          File.open(file_path, 'r') do |f|
+            f.each_line do |line|
+              puts line
+            end
+          end
+        end
       end
+      print ItChap2.new().reviews
     end
   end
 end

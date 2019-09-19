013bff15db9018c04eed0405c42bcb3ce738cef4
Hai <hai@iwa.fi>
2019-09-19 09:04:33 +0700

    add reason to use form object

--- Backend/Frameworks/Rails/BestPractices/form_object.rb
+++ Backend/Frameworks/Rails/BestPractices/form_object.rb
@@ -4,6 +4,7 @@ module Frameworks
       class FormObject
         def reasons_to_use
           <<~REASONS.strip
+            - extract the validations from Rails models
           REASONS
         end
       end

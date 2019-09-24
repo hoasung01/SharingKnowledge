bc83ea2ea89dc8d2c66d7f97c755ef32af3ca343
hainguyen <hai@iwa.fi>
2019-09-24 21:08:53 +0700

    add npm package to git pull multiple repos

---
+++ Git/TIL/Y2019/git_pull_all.rb
@@ -0,0 +1,13 @@
+module Git
+  module TIL
+    module Y2019
+      class GitPullAll
+        def reference
+          <<~REFERENCE
+            https://github.com/tatsuyaoiw/git-pull-all
+          REFERENCE
+        end
+      end
+    end
+  end
+end

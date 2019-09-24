6b5aa3b4605617a7e2e80da2ca8009714ce4105e
Nguyen Ngoc Hai <hainguyen@Nguyens-MacBook-Pro.local>
2019-09-24 20:29:44 +0700

    add a npm package to deal with bulk processing git repos

---
+++ Git/TIL/Y2019/bulk_processing_git_repos.rb
@@ -0,0 +1,13 @@
+module Git
+  module TIL
+  	module Y2019
+  		class BulkProcessingGitRepos
+  			def reference
+  				<<~REFERENCE
+  					https://github.com/pazdera/gitwalk
+  				REFERENCE
+  			end
+  		end
+  	end
+  end
+end

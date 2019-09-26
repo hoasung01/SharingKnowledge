4592ee1464b2c0e802ff6aac9f1b9b6d18980063
hainguyen <hai@iwa.fi>
2019-09-26 23:06:16 +0700

    add awesome terrform list

---
+++ OpenSources/awesome_terraform.rb
@@ -0,0 +1,9 @@
+module OpenSource
+  class AwesomeTerraform
+    def reference
+      <<~REFERENCE.strip
+        https://github.com/shuaibiyy/awesome-terraform
+      REFERENCE
+    end
+  end
+end

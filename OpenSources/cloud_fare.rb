6187c30951fb08039091bfe261b77fdc7a415e7f
hainguyen <hai@iwa.fi>
2019-09-26 17:10:14 +0700

    add cloud_fare as opensource on github

---
+++ OpenSources/cloud_fare.rb
@@ -0,0 +1,9 @@
+module OpenSource
+    class CloudFare
+        def reference
+            <<~REFERENCE.strip
+                https://github.com/cloudflare
+            REFERENCE
+        end
+    end
+end

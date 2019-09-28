1f89cd010d622b0caa53265e15217f49441d250b
hainguyen <hai@iwa.fi>
2019-09-28 20:36:42 +0700

    add Finnish dictionary

---
+++ SocialLife/Finnish/dictionary.rb
@@ -0,0 +1,28 @@
+module SocialLife
+  module Finnish
+    class Dictionary
+      # TODO: refactor duplicated code with Vietnamese
+      DICTIONARY_FILE = 'words.md'.freeze
+
+      def show
+        read_from("#{current_path}/#{DICTIONARY_FILE}")
+      end
+
+      private
+
+      def read_from(file_path)
+        File.open(file_path, 'r') do |f|
+          f.each_line do |line|
+            puts line
+          end
+        end
+      end
+
+      def current_path
+        File.expand_path File.dirname(__FILE__)
+      end
+    end
+
+    print Dictionary.new().show
+  end
+end

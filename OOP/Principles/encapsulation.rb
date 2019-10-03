e797bbfa971b1a198979da65fbde6761824e29fb
Rajagopal <psraja@thoughtworks.com>
2019-10-03 10:55:48 +0530

    Add DEFINE block at the end to handle unterminated string

--- OOP/Principles/encapsulation.rb
+++ OOP/Principles/encapsulation.rb
@@ -6,6 +6,7 @@ module OOP
         Encapsulation is the process of bundling data and the code 
         that operates on data together as part of a single entity(class).
         This prevents unauthorized access to the private data within the entity.
+        DEFINE
       end
     end
   end

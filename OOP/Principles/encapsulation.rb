187bcad1ceee432d37b91855eb8944dfef09bb26
Rajagopal <psraja@thoughtworks.com>
2019-10-03 16:58:57 +0530

    Formatted based on rubocop rules

--- OOP/Principles/encapsulation.rb
+++ OOP/Principles/encapsulation.rb
@@ -2,11 +2,11 @@ module OOP
   module Principles
     class Encapsulation
       def define
-        =begin
-        Encapsulation is the process of bundling data and the code 
-        that operates on data together as part of a single entity(class).
-        This prevents unauthorized access to the private data within the entity.
-        =end
+        <<~DEFINE.strip
+          Encapsulation is the process of bundling data and the code
+          that operates on data together as part of a single entity.
+          This prevents unauthorized access to the private data within the entity.
+          DEFINE
       end
     end
   end

680cc7623f37988415b064e5d9c9927389d16a29
Rajagopal <psraja@thoughtworks.com>
2019-10-03 11:01:32 +0530

    use begin and end block for the comments

--- OOP/Principles/encapsulation.rb
+++ OOP/Principles/encapsulation.rb
@@ -2,11 +2,11 @@ module OOP
   module Principles
     class Encapsulation
       def define
-        <<~define_strip
+        =begin
         Encapsulation is the process of bundling data and the code 
         that operates on data together as part of a single entity(class).
         This prevents unauthorized access to the private data within the entity.
-        DEFINE
+        =end
       end
     end
   end

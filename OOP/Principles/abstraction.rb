11298a594d61ba72a8d1e76ad42bc4e3ffa2bb7d
Amol Grover <29180487+frextrite@users.noreply.github.com>
2019-10-04 19:04:23 +0530

    Update abstraction.rb
    add a definition for abstraction (OOP concept)

--- OOP/Principles/abstraction.rb
+++ OOP/Principles/abstraction.rb
@@ -2,6 +2,12 @@ module OOP
   module Principles
     class Abstraction
       def define
+        <<~DEFINE.strip
+          Abstraction is the process of removing physical, spatial,
+          or temporal details or attributes in the study of
+          objects or systems in order to focus attention on
+          details of higher importance.
+        DEFINE
       end
     end
   end

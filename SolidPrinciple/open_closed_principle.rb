badf7dd6ac2def95ff76d17be080fff094a8596b
rajnish kumar <rajnish4unow@yahoo.com>
2019-10-04 22:15:57 +0530

    Define Open/Closed principle

--- SolidPrinciple/open_closed_principle.rb
+++ SolidPrinciple/open_closed_principle.rb
@@ -1,6 +1,12 @@
 module SolidPrinciple
   class OpenClosedPrinciple
     def define
+      <<~DEFINE.strip
+        The Open/Closed principle states software entities like classes,
+        modules, functions, etc. should be open for extension, but closed for
+        modification. that is, such an entity can allow its behaviour to be
+        extended without modifying its source code.
+      DEFINE
     end
   end
 end

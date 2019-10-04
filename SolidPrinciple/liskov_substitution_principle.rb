b2e66ffa116eaae3399dd2aeba853f4020b1b687
prambilla <56168772+prambilla@users.noreply.github.com>
2019-10-04 22:10:38 +0530

    Definition of Liskov Substitution Principle

--- SolidPrinciple/liskov_substitution_principle.rb
+++ SolidPrinciple/liskov_substitution_principle.rb
@@ -1,6 +1,7 @@
 module SolidPrinciple
   class LiskovSubstitutionPrinciple
     def define
+      Let Φ(x) be a property provable about objects x of type T. Then Φ(y) should be true for objects y of type S where S is a subtype of T.
     end
   end
 end

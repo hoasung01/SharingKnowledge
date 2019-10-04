cc9b18a11a7acbb32d14ab63e25c6f89182ebf02
prambilla <56168772+prambilla@users.noreply.github.com>
2019-10-04 22:13:09 +0530

    Definition of Dependency Inversion Principle

--- SolidPrinciple/dependency_inversion_princinple.rb
+++ SolidPrinciple/dependency_inversion_princinple.rb
@@ -1,6 +1,8 @@
 module SolidPrinciple
   class DependencyInversionPrinciple
     def define
+      High-level modules should not depend on low-level modules. Both should depend on abstractions.
+      Abstractions should not depend on details. Details should depend on abstractions.
     end
   end
 end

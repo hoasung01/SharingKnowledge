359d6508105c40d662679cb54e79e044c17696e9
hainguyen <hai@iwa.fi>
2019-10-10 17:54:15 +0700

    add linux folder structure and their meanings

---
+++ Computer/OperatingSystems/Linux/directory_structures.rb
@@ -0,0 +1,30 @@
+module Computer
+  module OperatingSystems
+    module Linux
+      class DirectoryStructures < Base
+        FILE_NAMES = {
+          '/bin'        => 'Essential User Binaries',
+          '/boot'       =>  'Static Boot Files',
+          '/cdrom'      => 'Historical Mount Point for CD-ROMs',
+          '/dev'        => 'Device Files',
+          '/etc'        => 'Configuration Files',
+          '/home'       => 'Home Folders',
+          '/lib'        => 'Essential Shared Libraries',
+          '/lost+found' => 'Recovered Files',
+          '/media'      => 'Removable Media',
+          '/mnt'        => 'Temporary Mount Points',
+          '/opt'        => 'Optional Packages',
+          '/proc'       => 'Kernel & Process Files',
+          '/root'       => 'Root Home Directory',
+          '/run'        => 'Application State Files',
+          '/sbin'       => 'System Administration Binaries',
+          '/selinux'    => 'SELinux Virtual File System',
+          '/srv'        => 'Service Data',
+          '/tmp'        => 'Temporary Files',
+          '/usr'        => 'User Binaries & Read-Only Data',
+          '/var'        => 'Variable Data Files'
+        }
+      end
+    end
+  end
+end

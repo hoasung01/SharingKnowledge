40719b8b4d44d83cddb8645277baad93699f40fb
hainguyen <hai@iwa.fi>
2019-10-12 14:18:34 +0700

    add software design and architecture road map reference link

---
+++ SoftwareDesign/base.rb
@@ -0,0 +1,9 @@
+module SoftwareDesign
+  class Base
+    def reference
+      <<~REFERENCE.strip
+        https://github.com/hoasung01/software-design-and-architecture-roadmap
+      REFERENCE
+    end
+  end
+end

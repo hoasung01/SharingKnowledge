ca2f20f95a2fbcf35792e8baffce6b916ee03a24
Nguyen Ngoc Hai <nguyenngochai.shipagent@gmail.com>
2020-12-11 09:30:14 +0700

    add more cryptocurrency

---
+++ CryptoCurrency/PiNetwork/base.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module CryptoCurrency
+  module PiNetwork
+    class Base
+      def reference
+        <<~REFERENCE.strip
+          https://minepi.com/
+        REFERENCE
+      end
+    end
+  end
+end
